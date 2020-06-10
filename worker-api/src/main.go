a90fbca7b05cb796ee5fa1e4b648f34ad7c784f9
Adrian Dinis <a44721@alunos.ipb.pt>
2020-06-10 14:15:23 +0100

    Implement worker-api daily scheduled task for ip retrieval

--- worker-api/src/main.go
+++ worker-api/src/main.go
@@ -3,11 +3,49 @@ package main
 import (
     "log"
     "net/http"
+    "io/ioutil"
+    "encoding/json"
     "github.com/gorilla/mux"
+    "github.com/jasonlvhit/gocron"
+
+    "./queue"
 )
+
+
+type Response struct {
+	Ips []string `json:"ips"`
+}
+
+func dailyScan() (err error) {
+    res, err := http.Get("http://gatherinfo-api:8080/assets/ips/public")
+
+    if err != nil {
+        log.Printf("Could not fetch public ips from API")
+        go dailyScan()
+
+        return
+    }
+
+    body, err := ioutil.ReadAll(res.Body)
+    if err != nil {
+        log.Printf("Could not read response from API")
+        return
+    }
+
+    var response Response
+    json.Unmarshal(body, &response)
+
+    for _, ip := range response.Ips {
+        queue.PublishScan("scans", ip + ":1-65535")
+    }
+    return
+}
 
 func main() {
     r := mux.NewRouter()
+
+    gocron.Every(1).Day().Do(dailyScan)
+    go dailyScan()
 
     // Misc
     r.HandleFunc("/status", QueueStatusCheck).
