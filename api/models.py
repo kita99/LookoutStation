bc9ffc8db9572ea4c4ebe08d170b2bbe66f73dc1
Nelson Cabete <nelsoncabeteff@icloud.com>
2020-06-11 00:16:19 +0100

    ADD: Email field for User

--- api/models.py
+++ api/models.py
@@ -27,6 +27,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True)
     password = db.Column(db.String(120), nullable=False)
+    email = db.Column(db.String(120), nullable=False)
 
     def as_dict(self):
         return serialize(self, ['password'])
