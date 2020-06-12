1f1d5644bdd5a3df7ce527977c936cd635e983bf
Adrian Dinis <a44721@alunos.ipb.pt>
2020-06-12 19:28:23 +0100

    [Frontend] Handle JWT expiry with axios interceptor

--- frontend/src/boot/axios.js
+++ frontend/src/boot/axios.js
@@ -1,4 +1,14 @@
 import Vue from 'vue'
 import axios from 'axios'
+import router from '../router'
+
+axios.interceptors.response.use(response => {
+  return response
+}, error => {
+  if (error.response.status === 401) {
+    router.push('/login')
+  }
+  return error
+})
 
 Vue.prototype.$axios = axios
