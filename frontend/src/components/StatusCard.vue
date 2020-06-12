9f959e6c995004ceff7e6504fb8159b81e4f2e2f
Adrian Dinis <a44721@alunos.ipb.pt>
2020-06-12 11:18:16 +0100

    [Frontend] Create status card component

---
+++ frontend/src/components/StatusCard.vue
@@ -0,0 +1,31 @@
+<template>
+    <q-card class="status-card-inner">
+      <q-card-section class="status-card-section-top">
+        <q-card class="status-icon-outer shadow-18">
+          <q-icon :name="icon" class="status-icon text-black"/>
+        </q-card>
+      </q-card-section>
+
+      <q-separator/>
+
+      <q-card-section class="status-card-section-bottom text-weight-thin">
+        <p>Last Update: 5 sec ago</p>
+      </q-card-section>
+    </q-card>
+</template>
+
+<script>
+export default {
+  name: 'StatusCard',
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    icon: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
