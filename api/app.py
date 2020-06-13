8520d5942ea2c9154d115355b6d0ef4fd635afb2
Nelson Cabete <nelsoncabeteff@icloud.com>
2020-06-13 01:03:25 +0100

    FIX: API 2nd Warning fixed

--- api/app.py
+++ api/app.py
@@ -4,4 +4,4 @@ import settings
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = settings.DB_CONN
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = 'False'
