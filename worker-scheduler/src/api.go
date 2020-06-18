70d17e1e8aa9690bfa77b4a81cafd09b26dc205a
Adrian Dinis <a44721@alunos.ipb.pt>
2020-06-18 06:04:50 +0100

    [worker-scheduler] Finish API integration

--- worker-scheduler/src/api.go
+++ worker-scheduler/src/api.go
@@ -4,11 +4,43 @@ import (
     "encoding/json"
     "io/ioutil"
     "net/http"
+    "strings"
     "bytes"
-    "fmt"
+    "log"
 )
 
-func PublishToQueue(queue string, message string) {
+type FeedsResponse struct {
+	Feeds []Feeds `json:"feeds"`
+}
+
+type Feeds struct {
+    ID           string `json:"id"`
+    Name         string `json:"name"`
+    Description  string `json:"description"`
+    Organization string `json:"organization"`
+    URL          string `json:"url"`
+    MetaURL      string `json:"meta_url"`
+}
+
+type PublicIPsResponse struct {
+    IPs []string `json: "ips"`
+}
+
+type FeedTask struct {
+    ByteSize int `json: "byte_size"`
+    CVEFeedId int `json: "cve_feed_id"`
+    SHA256 string `json: "sha256"`
+    CVEAmount int `json: "cve_amount"`
+    FeedModificationDate string `json: "feed_modification_date"`
+}
+
+type FeedSourceMetadata struct {
+    LastModifiedDate string
+    Size string
+    SHA256 string
+}
+
+func PublishToQueue(queue string, message string) bool {
     url := "http://lookoutstation-worker-master/publish"
     values := map[string]string{"queue": queue, "message": message}
     jsonRequest, _ := json.Marshal(values)
@@ -16,12 +48,103 @@ func PublishToQueue(queue string, message string) {
     resp, err := http.Post(url, "application/json", bytes.NewBuffer(jsonRequest))
 
     if err != nil {
-        fmt.Println("Could not communicate with master worker")
+        log.Println("Could not publish message to queue")
+        return false
     }
 
-    fmt.Println("response status:", resp.Status)
-    fmt.Println("response headers:", resp.Header)
+    if resp.Status != "200" {
+        return false
+    }
 
-    body, _ := ioutil.ReadAll(resp.Body)
-    fmt.Println("response body:", string(body))
+    return true
+}
+
+func GetPublicIPs() ([]string, error) {
+    var response PublicIPsResponse
+    res, err := http.Get("http://lookoutstation-api/assets/ips/public")
+
+    if err != nil {
+        log.Printf("Could not fetch ip list from API")
+        return response.IPs, err
+    }
+
+    body, _ := ioutil.ReadAll(res.Body)
+    err = json.Unmarshal(body, &response)
+
+    if err != nil {
+        log.Println("Could not process API response")
+        return response.IPs, err
+    }
+
+    return response.IPs, nil
+}
+
+func GetFeeds() ([]Feeds, error) {
+    var response FeedsResponse
+    res, err := http.Get("http://lookoutstation-api/feeds")
+
+    if err != nil {
+        log.Println("Could not fetch feeds from API")
+        return response.Feeds, err
+    }
+
+    body, _ := ioutil.ReadAll(res.Body)
+    err = json.Unmarshal(body, &response)
+
+    if err != nil {
+        log.Println("Could not process API response")
+        return response.Feeds, err
+    }
+
+    return response.Feeds, nil
+}
+
+func GetLastFeedTask(id string) (FeedTask, error) {
+	var feedTask FeedTask
+    res, err := http.Get("http://lookoutstation-api/feeds/" + id + "/tasks/latest")
+
+    if err != nil {
+        log.Printf("Could not fetch last feed task from API")
+        return feedTask, err
+    }
+
+    body, _ := ioutil.ReadAll(res.Body)
+	json.Unmarshal(body, &feedTask)
+
+    return feedTask, nil
+}
+
+func GetFeedSourceMetadata(metaURL string) (FeedSourceMetadata, error) {
+    var split []string
+    var feedSourceMetadata FeedSourceMetadata
+
+    res, err := http.Get(metaURL)
+
+    if err != nil {
+        log.Println("Could not fetch metadata from feed source")
+        return feedSourceMetadata, err
+    }
+
+    body, _ := ioutil.ReadAll(res.Body)
+    body_string := string(body)
+
+    lines := strings.Split(body_string, "\n")
+
+    for _, line := range lines {
+        split = strings.Split(line, ":")
+
+        if split[0] == "lastModifiedDate" {
+            feedSourceMetadata.LastModifiedDate = split[1]
+        }
+
+        if split[0] == "size" {
+            feedSourceMetadata.Size = split[1]
+        }
+
+        if split[0] == "sha256" {
+            feedSourceMetadata.SHA256 = split[1]
+        }
+    }
+
+    return feedSourceMetadata, nil
 }
