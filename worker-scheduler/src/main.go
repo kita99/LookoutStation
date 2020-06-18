6e5f3d395efc9a96e269ce1c84e0139d2b3980f9
Adrian Dinis <a44721@alunos.ipb.pt>
2020-06-18 06:07:12 +0100

    [worker-scheduler] Create endpoint for manual task triggers and minor improvements

--- worker-scheduler/src/main.go
+++ worker-scheduler/src/main.go
@@ -1,10 +1,28 @@
 package main
 
 import (
+    "net/http"
+    "log"
+
     "github.com/jasonlvhit/gocron"
+    "github.com/gorilla/mux"
 )
 
+func ManualTrigger(w http.ResponseWriter, r *http.Request) {
+    log.Println("Manually triggering all tasks")
+
+    go ScanAssets()
+    go ProcessAllFeeds()
+}
+
 func main() {
+    r := mux.NewRouter()
+
     gocron.Every(1).Day().Do(ScanAssets)
-    gocron.Every(1).Day().Do(FetchAllFeeds)
+    gocron.Every(1).Day().Do(ProcessAllFeeds)
+
+    r.HandleFunc("/trigger", ManualTrigger).
+        Methods("GET")
+
+    log.Fatal(http.ListenAndServe(":80", r))
 }
--- worker-scheduler/src/scanner.go
+++ worker-scheduler/src/scanner.go
@@ -3,40 +3,20 @@ package main
 import (
     "log"
     "time"
-    "net/http"
-    "io/ioutil"
-    "encoding/json"
 )
 
-type Response struct {
-	Ips []string `json:"ips"`
-}
-
-func ScanAssets() (err error) {
-    res, err := http.Get("http://lookoutstation-api/assets/ips/public")
+func ScanAssets() {
+    ips, err := GetPublicIPs()
 
     if err != nil {
-        log.Printf("Could not fetch ip list from API, retrying in 10 seconds")
+        log.Printf("Retrying in 10 seconds")
         time.Sleep(10 * time.Second)
         go ScanAssets()
 
         return
     }
 
-    body, err := ioutil.ReadAll(res.Body)
-    if err != nil {
-        log.Printf("Could not read response from API, retrying in 10 seconds")
-        time.Sleep(10 * time.Second)
-        go ScanAssets()
-
-        return
-    }
-
-    var response Response
-    json.Unmarshal(body, &response)
-
-    for _, ip := range response.Ips {
+    for _, ip := range ips {
         PublishToQueue("scans", ip + ":1-65535")
     }
-    return
 }
