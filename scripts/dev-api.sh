7ed4451944732ae58cf28c4e6478e915720498e5
Adrian Dinis <a44721@alunos.ipb.pt>
2020-06-19 16:52:40 +0100

    [API] Setup API package as editable for namespacing purposes, tweak dev script to cleanup side-effects

--- scripts/dev-api.sh
+++ scripts/dev-api.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 docker run --rm -it --env-file=$(pwd)/configs/postgres.env --name=lookoutstation-dev-api --net=lookoutstationnet -v $(pwd)/:/develop/ python:3.8 /bin/bash
+sudo rm -rf $(pwd)/api/lookoutstation_api.egg-info
