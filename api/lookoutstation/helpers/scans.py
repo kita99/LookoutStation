9b636ecab79ab12492fbbe5b67de18d1e8c6dd06
Adrian Dinis <a44721@alunos.ipb.pt>
2020-06-20 19:05:25 +0100

    [API] Implement scans endpoints along with some helper funcs

---
+++ api/lookoutstation/helpers/scans.py
@@ -0,0 +1,38 @@
+def __assign_values():
+    result = {
+        'range_start': port['id'],
+        'protocol': port['protocol'],
+        'state': port['state']['state'],
+        'reason': port['state']['reason'],
+        'service_name': port['service']['name']
+    }
+
+    return result
+
+
+def compact():
+    """
+        Takes in an a list of ports and attempts to compact them into port ranges
+        when certain conditions are met
+    """
+    result = []
+    
+    for i, port in enumerate(host['ports']):
+        if len(result) == 0:
+            result.append({})
+            result[-1] = __assign_values(port)
+
+            continue
+
+        if (
+            result[-1]['protocol'] != port['protocol'] or 
+            result[-1]['state'] != port['state']['state'] or
+            result[-1]['reason'] != port['state']['reason'] or
+            result[-1]['service_name'] != port['service']['name']
+        ):
+            result[-1]['range_end'] = host['ports'][i-1]['id']
+
+            result.append({})
+            result[-1] = __assign_values(port)
+
+    return result
--- api/lookoutstation/routes/scans.py
+++ api/lookoutstation/routes/scans.py
@@ -1,37 +1,136 @@
+from psycopg2.extras import NumericRange
+from sqlalchemy import desc
 from flask import Blueprint
 from flask import request
 
 from lookoutstation.helpers import authentication
-from lookoutstation.models import db
+from lookoutstation.helpers import scans
+from lookoutstation.models import Scan
+from lookoutstation.models import Port
+from lookoutstation.app import db
 
 
 scans = Blueprint('scans', __name__)
 
 
-@scans.route('/<public_ip>', methods=['PUT'])
-def add_scans(public_ip):
+@scans.route('/<public_ip>', methods=['POST'])
+def create_scan(public_ip):
     json_request = request.json
-    ports = json_request.get('ports')
 
-    if not isinstance(ports, list):
+    worker_code = json_request.get('worker_code')
+    payload = json_request.get('payload')
+
+    if not worker_code or not payload:
         return {'message': 'One or more parameters is malformed'}, 400
 
     try:
-        for port in ports:
-            scan = Scan(
-                public_ip=public_ip,
-                port=port['id'],
+        scan = Scan(
+            worker_code=worker_code,
+            public_ip=public_ip,
+            payload=payload,
+            progress=0
+        )
+
+        db.session.add(scan)
+        db.session.commit()
+
+        return {'message': 'Scan created successfully'}
+    except Exception as e:
+        print(e)
+        db.session.rollback()
+        return {'message': 'Internal server error'}, 500
+
+
+@scans.route('/<public_ip>', methods=['PUT'])
+def update_scan(public_ip):
+    is_port_range = False
+    json_request = request.json
+
+    worker_code = json_request.get('worker_code')
+    progress = json_request.get('progress')
+    ports = json_request.get('ports')
+    hosts = json_request.get('hosts')
+
+    if not isinstance(hosts, list):
+        return {'message': 'One or more parameters is malformed'}, 400
+
+    host = hosts[0]
+
+    if not worker_code or not progress or not ports:
+        return {'message': 'One or more parameters is malformed'}, 400
+
+    scan = Scan.query.filter_by(worker_code=worker_code).order_by(desc(Scan.updated_on)).first()
+
+    if not scan:
+        return {'message': 'This worker does not have any recently initiated scans'}, 404
+
+
+    if '-' in ports:
+        range_start, range_end = ports.split('-')
+        is_port_range = True
+
+
+    if host['ports'] and not host['extra_ports']:
+        compact_ports = scans.compact(host)
+
+        for port in compact_ports:
+            is_port_range = False
+
+            if 'range_end' in port:
+                is_port_range = port['range_start'] != port['range_end']
+
+            scan.ports.append(Port(
+                port=port['range_start'] if not is_port_range else None,
+                port_range=NumericRange(port['range_start'], port['range_end']) if is_port_range else None,
                 protocol=port['protocol'],
-                service_name=port['service']['name'],
-                state=port['state']['state'],
-                state_reason=port['state']['reason']
-            )
+                service_name=port['service_name'],
+                state=port['state'],
+                reason=port['reason']
+            ))
 
-            db.session.add(scan)
 
+    if not host['ports'] and host['extra_ports']:
+        scan.ports.append(Port(
+            port=ports if not is_port_range else None,
+            port_range=NumericRange(int(range_start), int(range_end)) if is_port_range else None,
+            protocol='tcp',
+            service_name=None,
+            state=host['extra_ports'][0]['state'],
+            reason=host['extra_ports'][0]['reasons'][0]['reason']
+        ))
+
+
+    if host['ports'] and host['extra_ports']:
+        for i, port in enumerate(host['ports']):
+            if i != 0:
+                range_start = int(host['ports'][i-1]['id']) + 1
+
+            scan.ports.append(Port(
+                port_range=NumericRange(int(range_start), int(port['id']) - 1),
+                protocol='tcp',
+                service_name=None,
+                state=host['extra_ports'][0]['state'],
+                reason=host['extra_ports'][0]['reasons'][0]['reason']
+            ))
+
+            scan.ports.append(Port(
+                port=port['id'],
+                protocol='tcp',
+                service_name=None,
+                state=host['extra_ports'][0]['state'],
+                reason=host['extra_ports'][0]['reasons'][0]['reason']
+            ))
+
+    try:
+        scan.progress = progress
+        scan.state = host['status']['state']
+        scan.reason = host['status']['reason']
+
+        db.session.add(scan)
         db.session.commit()
 
         return {'message': 'Scan data inserted successfully'}
     except:
         db.session.rollback()
         return {'message': 'Internal server error'}, 500
+
