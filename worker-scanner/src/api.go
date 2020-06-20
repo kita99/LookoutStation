ddd518ffdf044d8f7c96b55b2b3c9584fa264d7f
Adrian Dinis <a44721@alunos.ipb.pt>
2020-06-20 02:25:10 +0100

    [worker-scanner] Finish api integration

--- worker-scanner/src/api.go
+++ worker-scanner/src/api.go
@@ -1,21 +1,70 @@
 package main
 
 import (
+    "encoding/json"
     "net/http"
     "bytes"
+    "strings"
     "log"
 )
 
-func StoreScanResults(address string, scanResults []byte) bool  {
-    resp, err := http.Post("http://lookoustation-api/scans/" + address, "application/json", bytes.NewBuffer(scanResults))
+func UpdateScan(address string, results ScanResults) bool  {
+    scanResults, _ := json.Marshal(results)
+
+    client := &http.Client{}
+
+    req, err := http.NewRequest(http.MethodPut, "http://lookoutstation-api/scans/" + address, bytes.NewBuffer(scanResults))
 
     if err != nil {
-		log.Fatalf("Could not submit scan results: %v", err)
+		log.Printf("Could not submit scan results: %v", err)
         return false
     }
 
-    if resp.Status != "200" {
-		log.Fatalf("Could not submit scan results: %v", err)
+    req.Header.Set("Content-Type", "application/json; charset=utf-8")
+    resp, err := client.Do(req)
+
+    if err != nil {
+		log.Printf("Could not submit scan results: %v", err)
+        return false
+    }
+
+    if resp.StatusCode != 200 {
+		log.Printf("Could not submit scan results: %v", err)
+        return false
+    }
+
+    return true
+}
+
+func NotifyScanStart(workerID string, payload string) bool {
+    split := strings.Split(payload, ":")
+
+    request := struct {
+        WorkerID string `json:"worker_code"`
+        Payload string `json:"payload"`
+    }{
+        workerID,
+        payload,
+    }
+
+    notification, err := json.Marshal(request)
+
+    if err != nil {
+		log.Printf("Could not create json request: %v", err)
+        return false
+    }
+
+    resp, err := http.Post("http://lookoutstation-api/scans/" + split[0], "application/json", bytes.NewBuffer(notification))
+
+    if err != nil {
+		log.Printf("Could not notify scan start: %v", err)
+        return false
+    }
+
+    if resp.StatusCode != 200 {
+        log.Println(request)
+        log.Println(resp.Body)
+        log.Printf("Could not notify scan start, API returned: %v", resp.StatusCode)
         return false
     }
 
