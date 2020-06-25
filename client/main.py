de833dc4e6d13ddc9ad0dfa46208d96443f9a202
Nelson Cabete <nelsoncabeteff@icloud.com>
2020-06-25 22:13:59 +0100

    [client] PEP8 Compliant and Minor Wording Fix

--- client/main.py
+++ client/main.py
@@ -42,7 +42,6 @@ class Client():
 
         return kernel_version
 
-
     def __fetch_installed_packages(self):
         packages = self.instance.client.query('select name, version from deb_packages').response
 
@@ -54,34 +53,33 @@ class Client():
 
         return str(os_name) + ' ' + str(os_version)
 
-
     def upsert_device(self):
         _endpoint = f'{self.API_URL}/assets'
 
         insert_device = requests.post(_endpoint,
-                                    json = {
-                                        'uuid': self.uuid,
-                                        'hostname': self.hostname,
-                                        'operating_system': self.operating_system,
-                                        'kernel_version': self.kernel_version,
-                                        'private_ip': self.private_ip,
-                                        'public_ip': self.public_ip
-                                    }
-        )
+                                      json={
+                                          'uuid': self.uuid,
+                                          'hostname': self.hostname,
+                                          'operating_system': self.operating_system,
+                                          'kernel_version': self.kernel_version,
+                                          'private_ip': self.private_ip,
+                                          'public_ip': self.public_ip
+                                      }
+                                      )
 
         if insert_device.status_code == 201:
-            #logging.info(insert_software.text)
-            print("Yay, created device.")
+            # logging.info(insert_software.text)
+            print("Device created.")
             return True
 
         elif insert_device.status_code == 200:
-            #logging.error(insert_device.text)
-            print('Oh no, device already exists. Ignoring...')
+            # logging.error(insert_device.text)
+            print('Device with this UUID found.')
             return True
 
         elif insert_device.status_code == 500:
-            #logging.error(insert_device.text)
-            print(f'Something went wrong...No one likes exceptions!')
+            # logging.error(insert_device.text)
+            print('Something went wrong...Check the logs!')
             return False
 
         return None
@@ -90,40 +88,39 @@ class Client():
         _endpoint = f'{self.API_URL}/assets/{self.uuid}'
 
         insert_software = requests.put(_endpoint,
-                                    json = {
-                                        'software': self.installed_packages
-                                    }
-        )
+                                       json={
+                                           'software': self.installed_packages
+                                       }
+                                       )
 
         if insert_software.status_code == 201:
-            #logging.info(insert_software.text)
-            print("Yay, inserted software.")
+            # logging.info(insert_software.text)
+            print("Inserted software.")
             return True
 
         elif insert_software.status_code == 404:
-            #logging.error(insert_software.text)
+            # logging.error(insert_software.text)
             print('No device found with this UUID.')
             return False
 
         elif insert_software.status_code == 400:
-            #logging.error(insert_software.text)
+            # logging.error(insert_software.text)
             print('Invalid data passed.')
             return False
 
         elif insert_software.status_code == 500:
-            #logging.error(insert_software.text)
-            print('Something went wrong..No one likes exceptions!')
+            # logging.error(insert_software.text)
+            print('Something went wrong...Check the logs!')
             return False
-
 
     def process(self):
         if not self.upsert_device():
-            #logging.error('Something went wrong with creating the device...')
-            print('Something went wrong with creating the device...')
+            # logging.error('Something went wrong with creating the device...')
+            print('Something went wrong while attempting to create the device...')
 
         if not self.upsert_software():
-            #logging.error('Something went wrong with inserting the software...')
-            print('Something went wrong with inserting the software...')
+            # logging.error('Something went wrong with inserting the software...')
+            print('Something went wrong while attempting to insert the software...')
 
 
 client = Client()
