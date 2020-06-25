1e64e241f64e181fb680ab6c855c48afa476b45b
Adrian Dinis <a44721@alunos.ipb.pt>
2020-06-26 00:55:56 +0100

    [API] Finish statistics endpoint

--- api/lookoutstation/routes/statistics.py
+++ api/lookoutstation/routes/statistics.py
@@ -1,7 +1,7 @@
 from flask import Blueprint
-from flask import request
 
-from lookoutstation.models import Scan
+from lookoutstation.models import Software
+from lookoutstation.models import Asset
 from lookoutstation.models import Port
 from lookoutstation.app import db
 
@@ -11,9 +11,17 @@ statistics = Blueprint('statistics', __name__)
 
 @statistics.route('/overview', methods=['GET'])
 def get_overview_statistics():
+    vulnerability_count = 0
+
     try:
-        asset_count = 1
-        vulnerability_count = 'IMPLEMENT_ME'
+        asset_count = Asset.query.count()
+
+        global_software = Software.query.all()
+
+        for software in global_software:
+            if software.matched_cves:
+                vulnerability_count += len(software.matched_cves)
+
         open_port_count = Port.query.filter_by(state='open').filter(Port.port_range is None).count()
         open_port_ranges = Port.query.filter_by(state='open').filter(Port.port is None).all()
 
