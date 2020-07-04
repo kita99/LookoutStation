d0376a8ad97e38679604bacb0ea484d2cdf89034
Adrian Dinis <a44721@alunos.ipb.pt>
2020-07-04 01:40:55 +0100

    Improve folder structure, update .gitignore and scripts

--- scripts/dev-api.sh
+++ scripts/dev-api.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker run --rm -it --env-file=$(pwd)/configs/postgres.env -e "FLASK_APP=main.py" --name=lookoutstation-dev-api --net=lookoutstationnet -v $(pwd)/:/develop/ python:3.8 /bin/bash
-sudo rm -rf $(pwd)/api/lookoutstation_api.egg-info
+docker run --rm -it --env-file=$(pwd)/src/configs/postgres.env -e "FLASK_APP=main.py" --name=lookoutstation-api-dev --net=lookoutstationnet -v $(pwd)/src/api:/app python:3.8 /bin/bash
+sudo rm -rf $(pwd)/src/api/lookoutstation_api.egg-info
--- scripts/dev-frontend.sh
+++ scripts/dev-frontend.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --rm -it --env-file=$(pwd)/configs/postgres.env --name=lookoutstation-dev-frontend --net=lookoutstationnet -v $(pwd)/frontend:/develop/ node:lts-alpine /bin/bash
+docker run --rm -it --env-file=$(pwd)/src/configs/postgres.env --name=lookoutstation-frontend-dev --net=lookoutstationnet -v $(pwd)/src/frontend:/app node:lts-alpine /bin/bash
